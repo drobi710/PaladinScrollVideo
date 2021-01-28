f4b7e24bd13b5c66bcd272cf26177936f02c50fc
Danielle Robitshek <drobi710@gmail.com>
2021-01-27 20:20:01 -0500

    jpg and js added

---
+++ scripts.js
@@ -0,0 +1,40 @@
+const html = document.documentElement;
+const canvas = document.getElementById("paladinJump");
+const context = canvas.getContext("2d");
+
+const frameCount = 220;
+const currentFrame = (index) => `paladinJump${index.toString().padStart(4, "0")}.jpg`;
+
+const preloadImages = () => {
+  for (let i = 1; i < frameCount; i++) {
+    const img = new Image();
+    img.src = currentFrame(i);
+  }
+};
+
+const img = new Image();
+img.src = currentFrame(1);
+canvas.width = 720;
+canvas.height = 936;
+img.onload = function () {
+  context.drawImage(img, 0, 0);
+};
+
+const updateImage = (index) => {
+  img.src = currentFrame(index);
+  context.drawImage(img, 0, 0);
+};
+
+window.addEventListener("scroll", () => {
+  const scrollTop = html.scrollTop;
+  const maxScrollTop = html.scrollHeight - window.innerHeight;
+  const scrollFraction = scrollTop / maxScrollTop;
+  const frameIndex = Math.min(
+    frameCount - 1,
+    Math.ceil(scrollFraction * frameCount)
+  );
+
+  requestAnimationFrame(() => updateImage(frameIndex));
+});
+
+preloadImages();
